df4b6db168c256db48eb65ba131450db4009ead7
Qin Lin <47169253+Ci-TJ@users.noreply.github.com>
2023-07-11 14:18:11 +0800

    Update utile.py
    Add evaluation metrics.

--- utile.py
+++ utile.py
@@ -4,24 +4,27 @@ import pandas as pd
 from sklearn.metrics import roc_auc_score
 
 
-def calculate_metrics(y_true, y_pred):
+def calculate_metrics(y_true, y_pred, thres=0.5):
     TP = 0
     TN = 0
     FP = 0
     FN = 0
     for i in range(len(y_true)):
-        if y_true[i] == 1 and y_pred[i] >= 0.4:
+        if y_true[i] == 1 and y_pred[i] >= thres:
             TP += 1
-        if y_true[i] == 0 and y_pred[i] < 0.4:
+        if y_true[i] == 0 and y_pred[i] < thres:
             TN += 1
-        if y_true[i] == 0 and y_pred[i] >= 0.4:
+        if y_true[i] == 0 and y_pred[i] >= thres:
             FP += 1
-        if y_true[i] == 1 and y_pred[i] < 0.4:
+        if y_true[i] == 1 and y_pred[i] < thres:
             FN += 1
     sensitivity = TP / (TP + FN + 1e-10)
-    precision = TP / (TP + FP + 1e-10)
+    precision = TP / (TP + FP + 1e-10) #ppv
+    specificity = TN / (TN + FP + 1e-10)
+    npv = TN / (TN + FN + 1e-10)
+    acc = (TN + TP) / (TN + FN + TP + FP + 1e-10)
     F1_score = 2*(precision*sensitivity)/(precision + sensitivity + 1e-10)
-    return sensitivity, precision, F1_score
+    return sensitivity, precision, specificity, npv, acc, F1_score
 
 
 def get_result(loader, model):
@@ -34,8 +37,8 @@ def get_result(loader, model):
             pred += list(y_hat.cpu().numpy())
             target += list(y.cpu().numpy())
     auc = roc_auc_score(target, pred)
-    sen, pre, F1 = calculate_metrics(target, pred)
-    return auc, sen, pre, F1
+    sen, pre, spe, npv, acc, F1 = calculate_metrics(target, pred)
+    return auc, sen, pre, spe, npv, acc, F1
 
 
 
